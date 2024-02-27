323bd3cb4f856d370b1fae0769f72695f237676c
Gourab <gourabgaming111@gmail.com>
2024-02-27 19:08:03 +0530

    Added a graph to check the best fit line

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from LinearRegression import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
+import matplotlib.pyplot as plt
 
 X,y = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
 
@@ -18,3 +19,11 @@ def mse(ypred, y_test):
     return np.mean((ypred - y_test) ** 2)
 
 print(f"Mean Squared Error is: {mse(predictions, y_test)}")
+
+y_pred_line = reg.predict(X)
+cmap = plt.get_cmap('viridis')
+fig = plt.figure(figsize=(8,6))
+m1 = plt.scatter(X_train, y_train, color=cmap(0.9), s=10)
+m2 = plt.scatter(X_test, y_test, color=cmap(0.5), s=10)
+plt.plot(X, y_pred_line, color='black', linewidth=2, label='Prediction')
+plt.show()
